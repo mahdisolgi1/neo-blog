59eab1606f7457e824835bf223b8aaa971f96b3f
mahdisolgi1 <vass1383@gmail.com>
2024-10-17 22:20:22 +0330

    feat: Implement color system design from Figma and create Button component (styling pending)

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
-import { FC } from "react";
+import React from "react";
 
-const HomePage: FC = () => {
-   return <div>HomePage</div>;
+const HomePage: React.FC = () => {
+   return <div style={{ display: "flex", gap: "1rem", padding: "1rem" }}></div>;
 };
 
 export default HomePage;
