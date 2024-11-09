3ca6addaf574bf16bf26dd893953e09e303ee488
mahdisolgi1 <vass1383@gmail.com>
2024-11-09 21:02:43 +0330

    feat(BlogHighlightSection): make component dynamic and enhance hooks for article
    - Made BlogHighlightSection dynamic to render real-data
    - Updated hooks to fetch complete data for displaying all articles' details

--- frontend/src/components/BlogHighlightSection.tsx
+++ frontend/src/components/BlogHighlightSection.tsx
@@ -1,7 +1,26 @@
 import { FC } from "react";
 import styles from "./BlogHighlightSection.module.css";
+import { useGetArticles } from "@/services/generated/article/article";
+import getRandomObjects from "@/helpers/getRandomObjects";
 
 const BlogHighlightSection: FC = () => {
+   const { data } = useGetArticles();
+   const res = data?.data?.data;
+
+   const threeRandomArticle = getRandomObjects(res, 3);
+
+   const baseUrl = import.meta.env.VITE_BACK_END_BASE_URL;
+
+   const formatDate = (dateString?: string): string => {
+      if (!dateString) return "Invalid date";
+      const date = new Date(dateString);
+      return new Intl.DateTimeFormat("en-US", {
+         month: "long",
+         day: "numeric",
+         year: "numeric",
+      }).format(date);
+   };
+
    return (
       <>
          <div className={styles.BlogHighlightIntro}>
@@ -37,45 +56,74 @@ const BlogHighlightSection: FC = () => {
             </div>
          </div>
          <div className={styles.tweets}>
-            <div className={styles.tweet}>
-               <div className={styles.tweetProfile}>
-                  <div className={styles.tweetProfileDetails}>
-                     <img className={styles.tweetProfileImg} src="/John-Techson-big.png" alt="/John-Techson." />
-                     <div>
-                        <h4 className={styles.tweetProfileName}>John Techson</h4>
-                        <p className={styles.tweetProfileTopic}>Quantum Computing</p>
+            {threeRandomArticle.map((article) => (
+               <div key={article.id} className={styles.tweet}>
+                  <div className={styles.tweetProfile}>
+                     <div className={styles.tweetProfileDetails}>
+                        <img
+                           className={styles.tweetProfileImg}
+                           src={`${baseUrl}${article?.author?.avatar?.url}`}
+                           alt={article?.author?.name}
+                        />
+                        <div>
+                           <h4 className={styles.tweetProfileName}>{article?.author?.name}</h4>
+                           <p className={styles.tweetProfileTopic}>{article?.category?.name}</p>
+                        </div>
+                     </div>
+                     <div className={styles.tweetProfileCta}>
+                        <span className={styles.tweetProfileIconText}>View Blog</span>
+                        <img className={styles.tweetProfileIcon} src="/arrow-to-up-right.svg" alt="arrow-to-up-right" />
                      </div>
                   </div>
-                  <div className={styles.tweetProfileCta}>
-                     <span className={styles.tweetProfileIconText}>View Blog</span>
-                     <img className={styles.tweetProfileIcon} src="/arrow-to-up-right.svg" alt="arrow-to-up-right" />
+                  <div className={styles.tweetDetails}>
+                     <span className={styles.tweetDate}>{formatDate(article?.createdAt)}</span>
+                     <div className={styles.tweetText}>
+                        <h3 className={styles.tweetTextHeader}>{article?.title}</h3>
+                        <p className={styles.tweetTextP}>{article?.description}</p>
+                     </div>
+                     <div className={styles.tweetExtraInfos}>
+                        <div className={styles.tweetExtraInfo}>
+                           <img className={styles.tweetExtraInfoIcon} src="/heart-icon.svg" alt="heart-icon" />{" "}
+                           <span className={styles.tweetExtraInfoIconNum}>
+                              {article?.likes > 1000
+                                 ? `${
+                                      article.likes % 1000 === 0
+                                         ? article.likes / 1000
+                                         : (article.likes / 1000).toFixed(1)
+                                   }k`
+                                 : article?.likes}
+                           </span>
+                        </div>
+                        <div className={styles.tweetExtraInfo}>
+                           <img className={styles.tweetExtraInfoIcon} src="/comment-icon.svg" alt="comment-icon" />{" "}
+                           <span className={styles.tweetExtraInfoIconNum}>
+                              {" "}
+                              {article?.comments > 1000
+                                 ? `${
+                                      article.comments % 1000 === 0
+                                         ? article.comments / 1000
+                                         : (article.comments / 1000).toFixed(1)
+                                   }k`
+                                 : article?.comments}
+                           </span>
+                        </div>
+                        <div className={styles.tweetExtraInfo}>
+                           <img className={styles.tweetExtraInfoIcon} src="/share-icon.svg" alt="share-icon" />{" "}
+                           <span className={styles.tweetExtraInfoIconNum}>
+                              {" "}
+                              {article?.shares > 1000
+                                 ? `${
+                                      article.shares % 1000 === 0
+                                         ? article.shares / 1000
+                                         : (article.shares / 1000).toFixed(1)
+                                   }k`
+                                 : article?.shares}
+                           </span>
+                        </div>
+                     </div>
                   </div>
                </div>
-               <div className={styles.tweetDetails}>
-                  <span className={styles.tweetDate}>October 15, 2023</span>
-                  <div className={styles.tweetText}>
-                     <h3 className={styles.tweetTextHeader}>The Quantum Leap in Computing</h3>
-                     <p className={styles.tweetTextP}>
-                        Explore the revolution in quantum computing, its applications, and its potential impact on
-                        various industries.
-                     </p>
-                  </div>
-                  <div className={styles.tweetExtraInfos}>
-                     <div className={styles.tweetExtraInfo}>
-                        <img className={styles.tweetExtraInfoIcon} src="/heart-icon.svg" alt="heart-icon" />{" "}
-                        <span className={styles.tweetExtraInfoIconNum}>24.5k</span>
-                     </div>
-                     <div className={styles.tweetExtraInfo}>
-                        <img className={styles.tweetExtraInfoIcon} src="/comment-icon.svg" alt="comment-icon" />{" "}
-                        <span className={styles.tweetExtraInfoIconNum}>50</span>
-                     </div>
-                     <div className={styles.tweetExtraInfo}>
-                        <img className={styles.tweetExtraInfoIcon} src="/share-icon.svg" alt="share-icon" />{" "}
-                        <span className={styles.tweetExtraInfoIconNum}>20</span>
-                     </div>
-                  </div>
-               </div>
-            </div>
+            ))}
          </div>{" "}
          <div className={styles.BlogHighlightIntro}>
             <div className={styles.BlogHighlightHeading}>
