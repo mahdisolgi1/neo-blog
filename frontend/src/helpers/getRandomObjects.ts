5dd1b80010d9674c00f4cb3bb3a21f701d05c077
mahdisolgi1 <vass1383@gmail.com>
2024-11-11 00:04:15 +0330

    Fix getRandomObjects function

--- frontend/src/helpers/getRandomObjects.ts
+++ frontend/src/helpers/getRandomObjects.ts
@@ -1,8 +1,7 @@
 export default function getRandomObjects<T>(array: T[] | undefined, count: number): T[] {
-   if (!array || array.length === 0) {
-      return []; // Return an empty array if the input array is undefined or empty
+   if (!array?.length) {
+      return [];
    }
-
    const shuffledArray = array.sort(() => 0.5 - Math.random());
    const data = shuffledArray.slice(0, count);
 
