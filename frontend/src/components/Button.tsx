e4102ad0171f5bc81f98c08fdf2f83de3cd5c64c
mahdisolgi1 <vass1383@gmail.com>
2024-11-11 00:56:05 +0330

    fix a small bug in the button component

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -5,11 +5,13 @@ interface ButtonProps {
    type: "primary" | "secondary" | "tertiary";
    size: "sm" | "md" | "lg" | "xl" | "xxl";
    children: React.ReactNode;
+   className?: string;
 }
 
-const Button: FC<ButtonProps> = ({ type, size, children }) => {
-   const buttonClass = ` ${styles.button} ${styles[type]} ${styles[size]}`;
+const Button: FC<ButtonProps> = ({ type, size, children, className }) => {
+   const buttonClass = `${styles.button} ${styles[type]} ${styles[size]} ${className || ""}`;
 
    return <button className={buttonClass}>{children}</button>;
 };
+
 export default Button;
