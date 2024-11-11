7f44bc8ff029c9f19daad6bbfa66e482ac5e8c20
mahdisolgi1 <vass1383@gmail.com>
2024-11-11 03:42:47 +0330

    refactor: Move Resources component into Header for logical grouping
    - Moved `Resources` component from `heroSection` on the homepage to the `Header` component.
    - Removed `Resources` import from the homepage as it now belongs within the `Header` structure.
    - This change improves component organization by grouping `Resources` within `Header`, as they are conceptually related.
    - Updated relevant styles and layout adjustments to ensure smooth integration of `Resources` within `Header`.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,43 +1,47 @@
 import { FC } from "react";
 import styles from "./Header.module.scss";
+import Resources from "./Resources";
 
 const Header: FC = () => {
    return (
-      <header>
-         <div className={styles.container}>
-            <div className={styles.containerHeader}>
-               <h3 className={styles.containerSubHeading}>Your Journey to Tomorrow Begins Here</h3>
-               <h1 className={styles.containerHeading}>Explore the Frontiers of Artificial Intelligence</h1>
-               <p className={styles.containerP}>
-                  Welcome to the epicenter of AI innovation. FutureTech AI News is your passport to a world where
-                  machines think, learn, and reshape the future.
-               </p>
+      <>
+         <header>
+            <div className={styles.container}>
+               <div className={styles.containerHeader}>
+                  <h3 className={styles.containerSubHeading}>Your Journey to Tomorrow Begins Here</h3>
+                  <h1 className={styles.containerHeading}>Explore the Frontiers of Artificial Intelligence</h1>
+                  <p className={styles.containerP}>
+                     Welcome to the epicenter of AI innovation. FutureTech AI News is your passport to a world where
+                     machines think, learn, and reshape the future.
+                  </p>
+               </div>
+               <div className={styles.containerDetails}>
+                  <div className={styles.containerDetail}>
+                     <div>
+                        <h2 className={styles.containerDetailHeading}>300</h2>
+                        <span className={styles.containerDetailPlus}>+</span>
+                     </div>
+                     <p className={styles.containerDetailP}>Resources available</p>
+                  </div>
+                  <div className={styles.containerDetail}>
+                     <div>
+                        <h2 className={styles.containerDetailHeading}>12k</h2>
+                        <span className={styles.containerDetailPlus}>+</span>
+                     </div>
+                     <p className={styles.containerDetailP}>Total Downloads</p>
+                  </div>
+                  <div className={styles.containerDetail}>
+                     <div>
+                        <h2 className={styles.containerDetailHeading}>10k</h2>
+                        <span className={styles.containerDetailPlus}>+</span>
+                     </div>
+                     <p className={styles.containerDetailP}>Active Users</p>
+                  </div>
+               </div>
             </div>
-            <div className={styles.containerDetails}>
-               <div className={styles.containerDetail}>
-                  <div>
-                     <h2 className={styles.containerDetailHeading}>300</h2>
-                     <span className={styles.containerDetailPlus}>+</span>
-                  </div>
-                  <p className={styles.containerDetailP}>Resources available</p>
-               </div>
-               <div className={styles.containerDetail}>
-                  <div>
-                     <h2 className={styles.containerDetailHeading}>12k</h2>
-                     <span className={styles.containerDetailPlus}>+</span>
-                  </div>
-                  <p className={styles.containerDetailP}>Total Downloads</p>
-               </div>
-               <div className={styles.containerDetail}>
-                  <div>
-                     <h2 className={styles.containerDetailHeading}>10k</h2>
-                     <span className={styles.containerDetailPlus}>+</span>
-                  </div>
-                  <p className={styles.containerDetailP}>Active Users</p>
-               </div>
-            </div>
-         </div>
-      </header>
+            <Resources />
+         </header>
+      </>
    );
 };
 
