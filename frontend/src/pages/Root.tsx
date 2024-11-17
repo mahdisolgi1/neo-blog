eaa0ce2fa3237ab29c23e7883f8efbf885fdd2cb
mahdisolgi1 <vass1383@gmail.com>
2024-11-17 06:02:47 +0330

    chore: move Footer and FutureTechRevolutionSection components to Root directory

--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -2,15 +2,17 @@ import { FC } from "react";
 import Nav from "@/components/Nav";
 import { Outlet } from "react-router-dom";
 import TopBanner from "@/components/TopBanner";
+import FutureTechRevolutionSection from "@/components/FutureTechRevolutionSection";
+import Footer from "@/components/Footer";
 
 const Root: FC = () => {
    return (
       <>
          <TopBanner />
          <Nav />
-         <div>
-            <Outlet />
-         </div>
+         <Outlet />
+         <FutureTechRevolutionSection />
+         <Footer />
       </>
    );
 };
