b34340d911b945f52b0a7eb674c8ff992636a1ca
mahdisolgi1 <vass1383@gmail.com>
2024-11-17 21:07:28 +0330

    feat(Nav): add functionality for hamburgar menu to display links for mobile

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,21 +1,31 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import styles from "./Nav.module.scss";
 import { Link, useLocation } from "react-router-dom";
 import { HiOutlineMenuAlt3 } from "react-icons/hi";
 import Button from "./Button";
+import { IoClose } from "react-icons/io5";
 
 const Nav: FC = () => {
    const location = useLocation();
+   const [menuOpen, setMenuOpen] = useState(false);
+
+   const toggleMenu = () => {
+      console.log("fagds");
+      setMenuOpen((prev) => !prev);
+   };
+
+   const closeMenu = () => setMenuOpen(false);
 
    return (
       <div className={styles.navBar}>
          <img className={styles.logo} src="/logo.svg" alt="logo" />
          <nav className={styles.navBarLinksBox}>
-            <ul className={styles.navBarLinks}>
+            <ul className={`${styles.navBarLinks} ${menuOpen ? styles.navBarLinksMobile : ""}`}>
                <li className={styles.navBarLinkItem}>
                   <Link
                      to="/home"
                      className={`${styles.navBarLink} ${location.pathname === "/home" ? styles.activeLink : ""}`}
+                     onClick={closeMenu}
                   >
                      Home
                   </Link>
@@ -23,6 +33,7 @@ const Nav: FC = () => {
                <li className={styles.navBarLinkItem}>
                   <Link
                      to="/news"
+                     onClick={closeMenu}
                      className={`${styles.navBarLink} ${location.pathname === "/news" ? styles.activeLink : ""}`}
                   >
                      News
@@ -31,6 +42,7 @@ const Nav: FC = () => {
                <li className={styles.navBarLinkItem}>
                   <Link
                      to="/podcasts"
+                     onClick={closeMenu}
                      className={`${styles.navBarLink} ${location.pathname === "/podcasts" ? styles.activeLink : ""}`}
                   >
                      Podcasts
@@ -39,6 +51,7 @@ const Nav: FC = () => {
                <li className={styles.navBarLinkItem}>
                   <Link
                      to="/resources"
+                     onClick={closeMenu}
                      className={`${styles.navBarLink} ${location.pathname === "/resources" ? styles.activeLink : ""}`}
                   >
                      Resources
@@ -47,13 +60,25 @@ const Nav: FC = () => {
             </ul>
          </nav>
 
-         <HiOutlineMenuAlt3 color="white" className={styles.hamburgarMenu} aria-label="hamburgar-menu" />
+         {menuOpen ? (
+            <IoClose
+               color="white"
+               aria-label="close-hamburgar-menu"
+               onClick={closeMenu}
+               className={styles.hamburgarMenu}
+            />
+         ) : (
+            <HiOutlineMenuAlt3
+               onClick={toggleMenu}
+               color="white"
+               className={styles.hamburgarMenu}
+               aria-label="hamburgar-menu"
+            />
+         )}
 
-         <Link to="/contact" className={styles.ctaBtnLink}>
-            <Button size="md" type="primary" className={styles.ctaBtn}>
-               Contact Us
-            </Button>
-         </Link>
+         <Button size="md" type="primary" className={styles.ctaBtn}>
+            Contact Us
+         </Button>
       </div>
    );
 };
