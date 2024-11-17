9081827c6d0dddecf10df06638885ecfb49669d3
mahdisolgi1 <vass1383@gmail.com>
2024-11-17 07:15:59 +0330

    feat(Root): use scroll to up everytime url changes

--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -1,11 +1,16 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import Nav from "@/components/Nav";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import TopBanner from "@/components/TopBanner";
 import FutureTechRevolutionSection from "@/components/FutureTechRevolutionSection";
 import Footer from "@/components/Footer";
 
 const Root: FC = () => {
+   const { pathname } = useLocation();
+
+   useEffect(() => {
+      window.scrollTo(0, 0);
+   }, [pathname]);
    return (
       <>
          <TopBanner />
