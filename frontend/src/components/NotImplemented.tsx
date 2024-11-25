c45001380eb10a8b3cf06ef55238ae76958911c4
mahdisolgi1 <vass1383@gmail.com>
2024-11-25 17:53:09 +0330

    feat(NotImplemented): create NotImplemented component

---
+++ frontend/src/components/NotImplemented.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import styles from "./NotImplemented.module.scss";
+
+const NotImplemented: FC = () => {
+   return <div>NotImplemented</div>;
+};
+
+export default NotImplemented;
