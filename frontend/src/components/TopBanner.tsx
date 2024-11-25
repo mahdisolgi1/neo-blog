0511befb643b4c40fb95e0c1d46d734c1c8b5d07
mahdisolgi1 <vass1383@gmail.com>
2024-11-25 19:26:28 +0330

    feat: add to NotImplemented page for links used in every components and styles needed for them

--- frontend/src/components/TopBanner.tsx
+++ frontend/src/components/TopBanner.tsx
@@ -1,13 +1,14 @@
 import { FC } from "react";
 import styles from "./TopBanner.module.scss";
 import { PiArrowUpRight } from "react-icons/pi";
+import { Link } from "react-router-dom";
 
 const TopBanner: FC = () => {
    return (
-      <section className={styles.topBannerSection}>
+      <Link to="/not-implemented" className={styles.topBannerSection}>
          <p className={styles.topBannerP}>Subscribe to our Newsletter For Blogs and Resources</p>
          <PiArrowUpRight className={styles.topBannerIcon} color="gold" aria-label="arrow-to-to-right-icon" />
-      </section>
+      </Link>
    );
 };
 
