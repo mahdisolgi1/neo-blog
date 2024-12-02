7fbed7e015b8a9abacd3c06c261b481b14c38858
mahdisolgi1 <vass1383@gmail.com>
2024-12-02 13:32:50 +0330

    fix(Button): fix type prop to variant because of conflict

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -2,14 +2,14 @@ import { FC, ButtonHTMLAttributes } from "react";
 import styles from "./Button.module.scss";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-   type: "primary" | "secondary" | "tertiary";
+   variant: "primary" | "secondary" | "tertiary";
    size: "sm" | "md" | "lg" | "xl" | "xxl";
    children: React.ReactNode;
    className?: string;
 }
 
-const Button: FC<ButtonProps> = ({ type, size, children, className = "", onClick, ...rest }) => {
-   const buttonClass = `${styles.button} ${styles[type]} ${styles[size]} ${className}`.trim();
+const Button: FC<ButtonProps> = ({ variant, size, children, className = "", onClick, ...rest }) => {
+   const buttonClass = `${styles.button} ${styles[variant]} ${styles[size]} ${className}`.trim();
 
    return (
       <button onClick={onClick} className={buttonClass} {...rest}>
