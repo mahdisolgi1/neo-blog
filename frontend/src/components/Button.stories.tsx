1b9f5a4a956d93b0be0e493446ab11898d6ff77f
mahdisolgi1 <vass1383@gmail.com>
2024-12-02 13:51:50 +0330

    refactor(Button.stories): rename title and remove comments

--- frontend/src/components/Button.stories.tsx
+++ frontend/src/components/Button.stories.tsx
@@ -2,7 +2,7 @@ import { Meta, StoryFn } from "@storybook/react";
 import Button from "./Button";
 
 export default {
-   title: "Components/Button",
+   title: "UI/Button",
    component: Button,
    args: {
       children: "Click Me",
@@ -14,7 +14,7 @@ const Template: StoryFn<typeof Button> = (args) => <Button {...args} />;
 export const Default = Template.bind({});
 Default.args = {
    children: "Default Button",
-   type: "primary",
+   variant: "primary",
 };
 
 export const WithAction = Template.bind({});
