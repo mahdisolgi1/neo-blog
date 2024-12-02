61f25125b237edf3c1d0225ac79e0a426eae9824
mahdisolgi1 <vass1383@gmail.com>
2024-12-02 18:08:56 +0330

    feat(orval.config): integrate production server to config

--- frontend/orval.config.ts
+++ frontend/orval.config.ts
@@ -1,18 +1,16 @@
-import {defineConfig} from "orval";
-
+import { defineConfig } from "orval";
 
 export default defineConfig({
-    NeoBlog: {
-        output: {
-            mode: "tags-split",
-            target: "src/services/generated/index.ts",
-            schemas: "src/services/generated/models",
-            client: "swr",
-            baseUrl:"http://localhost:1337/api"
-        },
-        input: {
-            target: "../backend/src/extensions/documentation/documentation/1.0.0/full_documentation.json",
-        },
-    },
-
+   NeoBlog: {
+      output: {
+         mode: "tags-split",
+         target: "src/services/generated/index.ts",
+         schemas: "src/services/generated/models",
+         client: "swr",
+         baseUrl: "https://neo-blog-production.up.railway.app/api",
+      },
+      input: {
+         target: "../backend/src/extensions/documentation/documentation/1.0.0/full_documentation.json",
+      },
+   },
 });
