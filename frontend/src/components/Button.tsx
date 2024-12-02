68b1e0108ed372199bf1e284ce3cf1bba14d6e38
mahdisolgi1 <vass1383@gmail.com>
2024-12-02 13:31:32 +0330

    feat(Button): extend Button props to extend HTMLButtonElement

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,17 +1,15 @@
-import { FC } from "react";
+import { FC, ButtonHTMLAttributes } from "react";
 import styles from "./Button.module.scss";
 
-interface ButtonProps {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
    type: "primary" | "secondary" | "tertiary";
    size: "sm" | "md" | "lg" | "xl" | "xxl";
    children: React.ReactNode;
    className?: string;
-   onClick?: () => void;
-   [key: string]: any;
 }
 
-const Button: FC<ButtonProps> = ({ type, size, children, className, onClick, ...rest }) => {
-   const buttonClass = `${styles.button} ${styles[type]} ${styles[size]} ${className || ""}`;
+const Button: FC<ButtonProps> = ({ type, size, children, className = "", onClick, ...rest }) => {
+   const buttonClass = `${styles.button} ${styles[type]} ${styles[size]} ${className}`.trim();
 
    return (
       <button onClick={onClick} className={buttonClass} {...rest}>
