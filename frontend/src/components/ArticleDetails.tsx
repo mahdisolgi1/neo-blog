77ec234c5ab2a0e84fee7f181a4b97f4ee8c9b23
MahdiSolgi <vass1383@gmail.com>
2025-01-02 06:43:51 -0800

    refactor(ArticleDetails): change fetching all articles then filtering it to dynamic fetching based on document id

--- frontend/src/components/ArticleDetails.tsx
+++ frontend/src/components/ArticleDetails.tsx
@@ -1,107 +1,86 @@
-import { useGetArticles } from "@/services/generated/article/article";
-import { Article, ArticleCategory } from "@/services/generated/models";
 import { FC, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useGetArticlesId } from "@/services/generated/article/article";
+import { Article, ArticleCategory } from "@/services/generated/models";
 import styles from "./ArticleDetails.module.scss";
 import formatDate from "@/helpers/formatDate";
-import { FaRegComment, FaRegHeart } from "react-icons/fa";
-import { BsSend } from "react-icons/bs";
+import toast from "react-hot-toast";
+import ArticleActions from "./ArticleActions";
+
 interface ArticleWithCustomization extends Article {
    category?: ArticleCategory & { name?: string };
 }
+
 const ArticleDetails: FC = () => {
    const [article, setArticle] = useState<ArticleWithCustomization | null>(null);
    const { id } = useParams<{ id: string }>();
 
-   const { data: articlesData, error: articleError } = useGetArticles({
-      populate: ["category", "cover", "author.avatar"],
-   });
-
+   const { data: articlesData, error: articleError } = useGetArticlesId(id || "");
+   const baseUrl = import.meta.env.VITE_BACK_END_BASE_URL;
+   console.log(articlesData);
    useEffect(() => {
-      if (articleError) {
-         console.error(articleError);
-      } else if (id && articlesData?.data?.data) {
-         const matchingArticle = articlesData.data.data.find((article) => String(article.id) === id);
-         setArticle(matchingArticle || null);
+      if (!id) {
+         toast.error("Error: Article ID is missing!");
+         return;
       }
-   }, [articlesData, articleError, id]);
-   console.log(article);
+
+      if (articleError) {
+         toast.error("Error loading the article. Please try again later.");
+         return;
+      }
+
+      if (articlesData?.data?.data) {
+         setArticle(articlesData?.data?.data);
+      } else {
+         toast.error("No matching article found!");
+      }
+   }, [id, articlesData, articleError]);
 
    if (!id) {
-      return <div>Error: Article ID is missing!</div>;
+      return <div className={styles.error}>Error: Article ID is missing!</div>;
    }
 
    if (!article) {
-      return <div>Loading or no matching article found...</div>;
+      return <div className={styles.loading}>Loading article...</div>;
    }
-   const baseUrl = import.meta.env.VITE_BACK_END_BASE_URL;
+   console.log(article);
 
    return (
-      <>
-         <section className={styles.articleDetailsSection}>
-            <img
-               src={article?.cover?.url ? `${baseUrl}${article.cover.url}` : "/cover-placeholder.svg"}
-               alt={article.cover?.name}
-               className={styles.cover}
-            />
-            <div className={styles.articleDetails}>
-               <div className={styles.articleDetailsHeader}>
-                  <div className={styles.articleDetailsHeaderProfile}>
-                     <img
-                        src={
-                           article?.author?.avatar?.url ? `${baseUrl}${article.author.avatar.url}` : "/emptyAuthor.svg"
-                        }
-                        alt={article?.author?.name}
-                        className={styles.authorAvatar}
-                     />
-                     <div>
-                        <h4 className={styles.author}>{article?.author?.name}</h4>
-                        <p className={styles.category}>{article?.category?.name}</p>
-                     </div>
-                  </div>
-                  <div className={styles.createdAtAndInfosBox}>
-                     <p className={styles.createdAt}>{formatDate(article?.createdAt)}</p>
-                     <div className={styles.tweetExtraInfos}>
-                        <div className={styles.tweetExtraInfo}>
-                           <FaRegHeart className={styles.tweetExtraInfoIcon} color="grey" aria-label="heart-icon" />
-                           <span className={styles.tweetExtraInfoIconNum}>
-                              {Number(article?.likes) && Number(article?.likes) > 1000
-                                 ? `${(Number(article.likes) / 1000).toFixed(
-                                      Number(article.likes) % 1000 === 0 ? 0 : 1
-                                   )}k`
-                                 : Number(article?.likes)}
-                           </span>
-                        </div>
-                        <div className={styles.tweetExtraInfo}>
-                           <FaRegComment className={styles.tweetExtraInfoIcon} color="grey" aria-label="comment-icon" />
-                           <span className={styles.tweetExtraInfoIconNum}>
-                              {Number(article?.comments) && Number(article?.comments) > 1000
-                                 ? `${(Number(article.comments) / 1000).toFixed(
-                                      Number(article.comments) % 1000 === 0 ? 0 : 1
-                                   )}k`
-                                 : Number(article?.comments)}
-                           </span>
-                        </div>
-                        <div className={styles.tweetExtraInfo}>
-                           <BsSend className={styles.tweetExtraInfoIcon} color="grey" aria-label="share-icon" />
-                           <span className={styles.tweetExtraInfoIconNum}>
-                              {Number(article?.shares) && Number(article?.shares) > 1000
-                                 ? `${(Number(article.shares) / 1000).toFixed(
-                                      Number(article.shares) % 1000 === 0 ? 0 : 1
-                                   )}k`
-                                 : Number(article?.shares)}
-                           </span>
-                        </div>
-                     </div>{" "}
+      <section className={styles.articleDetailsSection}>
+         <img
+            src={article.cover?.url ? `${baseUrl}${article.cover.url}` : "/cover-placeholder.svg"}
+            alt={article.cover?.name || "Cover image"}
+            className={styles.cover}
+         />
+         <div className={styles.articleDetails}>
+            <div className={styles.articleDetailsHeader}>
+               <div className={styles.articleDetailsHeaderProfile}>
+                  <img
+                     src={article.author?.avatar?.url ? `${baseUrl}${article.author.avatar.url}` : "/emptyAuthor.svg"}
+                     alt={article.author?.name || "Author"}
+                     className={styles.authorAvatar}
+                  />
+                  <div>
+                     <h4 className={styles.author}>{article.author?.name}</h4>
+                     <p className={styles.category}>{article.category?.name}</p>
                   </div>
                </div>
-               <div className={styles.article}>
-                  <h3 className={styles.title}>{article?.title}</h3>
-                  <p className={styles.description}>{article?.description}</p>
+               <div className={styles.createdAtAndInfosBox}>
+                  <p className={styles.createdAt}>{formatDate(article.createdAt)}</p>
+                  <ArticleActions
+                     documentId={article?.documentId}
+                     likes={article.likes}
+                     comments={article.comments}
+                     shares={article.shares}
+                  />
                </div>
             </div>
-         </section>
-      </>
+            <div className={styles.article}>
+               <h3 className={styles.title}>{article.title}</h3>
+               <p className={styles.description}>{article.description}</p>
+            </div>
+         </div>
+      </section>
    );
 };
 
