122b0177c420c210849948bbe83620fb2a27ccfd
MahdiSolgi <vass1383@gmail.com>
2025-01-11 05:50:38 -0800

    feat(NotFound): add className and used Btn as a link

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
 import { FC } from "react";
 import styles from "./NotFound.module.scss";
+import Button from "@/components/Button";
 const NotFound: FC = () => {
    return (
       <div className={styles.container}>
-         <h2>404 - Not Found</h2>
-         <p>The page you're looking for does not exist.</p>
-         <Link to="/">Go to Home</Link>
+         <h2 className={styles.header}>404 - Not Found</h2>
+         <p className={styles.p}>The page you're looking for does not exist.</p>
+         <Link to="/">
+            <Button size="md" variant="primary">
+               Go to Home
+            </Button>
+         </Link>
       </div>
    );
 };
